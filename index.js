a736a5fdee5a274c0fe1f03d399bdaf65020d06f
guavaguy2693 <sidhya.v.singh@gmail.com>
2025-09-13 16:49:57 +0530

    doned and dusted

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+
+const checker = /^(https?:\/\/)([a-zA-Z0-9-]+.)[a-zA-Z]{2,}.[a-z]{2,}/
+var MEMORY = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +25,24 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/shorturl/:url',
+	(req, res) => {
+		res.redirect(MEMORY[req.params.url]['original_url']);
+	}
+);
+
+app.post('/api/shorturl',
+	(req, res) => {
+		const url = req.body.url;
+		if (!(checker.test(url))) {
+			return res.json({'error': 'Invalid URL'});
+		}
+		var item = { original_url: url, short_url: MEMORY.length}
+		MEMORY.push(item);
+		res.json(item);
+	}
+);
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
